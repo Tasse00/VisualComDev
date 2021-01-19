7a961fcc30967c519986efb0606a1b1273b8ce8c
zhangzheng <zhangzheng@aengine.com.cn>
2021-01-19 14:38:19 +0800

    update index view

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,7 +3,7 @@ import {
   VisualDispatcherContext,
   useVisual,
 } from '@/components/Visual';
-import React, { useReducer, useMemo } from 'react';
+import React, { useReducer, useMemo, useEffect } from 'react';
 import styles from './index.less';
 import Fixed from '@/components/layout/Fixed';
 import Stretch from '@/components/layout/Stretch';
@@ -19,112 +19,121 @@ import WidgetVisualNode from '@/components/WidgetVisualNode';
 import Panel from '@/components/Panel';
 import LogsView from '@/components/Logger/LogsView';
 import { globalLoggerStore } from '@/components/Globals';
-
+import ComponentLibGallery from '@/components/ComponentLib/ComLibGallery';
+import ComponentLibProvider from '@/components/ComponentLib/ComponentLibProvider';
+import PropertyEditorLibProvider from '@/components/PropertyEditorLib/PropertyEditorLibProvider';
 export default () => {
-  const [state, dispatch] = useVisual();
+  // const [state, dispatch] = useVisual();
 
-  const tree = useMemo(() => convertTree(state), [
-    state.widgets,
-    state.childrenMap,
-    state.rootId,
-  ]);
+  // const tree = useMemo(() => convertTree(state), [
+  //   state.widgets,
+  //   state.childrenMap,
+  //   state.rootId,
+  // ]);
 
   const [ctxMenuState, ctxMenuControl] = useContextMenu();
 
+  // Property Editor Lib
+
+
   return (
-    <VisualDispatcherContext.Provider value={dispatch}>
-      <DndProvider backend={HTML5Backend}>
-        <ContextMenuContext.Provider value={ctxMenuControl}>
-          <div className={styles['app']}>
-            {/* 顶部菜单 */}
-            <Fixed
-              defaultSize={48}
-              position={'bottom'}
-              style={{ display: 'flex', alignItems: 'center' }}
-            >
-              <Panel style={{ padding: 8 }}>
-                <Toolbar
+    // <VisualDispatcherContext.Provider value={dispatch}>
+    <ComponentLibProvider>
+      <PropertyEditorLibProvider>
+        <DndProvider backend={HTML5Backend}>
+          <ContextMenuContext.Provider value={ctxMenuControl}>
+            <div className={styles['app']}>
+              {/* 顶部菜单 */}
+              <Fixed
+                defaultSize={48}
+                position={'bottom'}
+                style={{ display: 'flex', alignItems: 'center' }}
+              >
+                <Panel style={{ padding: 8 }}>
+                  {/* <Toolbar
                   rootId={state.rootId}
                   widgets={state.widgets}
                   childrenMap={state.childrenMap}
-                />
-              </Panel>
-            </Fixed>
-
-            <Stretch style={{ display: 'flex' }}>
-              {/* 组件画廊 */}
-              <Fixed defaultSize={160} position={'right'}>
-                <WidgetGallery />
+                /> */}
+                </Panel>
               </Fixed>
 
-              <Stretch
-                style={{
-                  display: 'flex',
-                  flexDirection: 'column',
-                  justifyContent: 'stretch',
-                  alignItems: 'stretch',
-                }}
-              >
-                {/* 可视编辑 */}
+              <Stretch style={{ display: 'flex' }}>
+                {/* 组件画廊 */}
+                <Fixed defaultSize={160} position={'right'}>
+                  {/* <WidgetGallery /> */}
+                  <ComponentLibGallery />
+                </Fixed>
+
                 <Stretch
                   style={{
-                    position: 'relative',
-                    padding: 32,
-                    backgroundColor: 'rgba(200,200,200,0.2)',
+                    display: 'flex',
+                    flexDirection: 'column',
+                    justifyContent: 'stretch',
+                    alignItems: 'stretch',
                   }}
                 >
-                  {/* {renderNode(tree, 1)} */}
-                  <WidgetVisualNode
+                  {/* 可视编辑 */}
+                  <Stretch
+                    style={{
+                      position: 'relative',
+                      padding: 32,
+                      backgroundColor: 'rgba(200,200,200,0.2)',
+                    }}
+                  >
+                    {/* <WidgetVisualNode
                     node={tree}
                     hoverId={state.hoverId}
                     selectedIds={state.selectedIds}
-                  />
-                </Stretch>
-                {/* 底部信息 */}
-                <Fixed defaultSize={200} position={'top'}>
+                  /> */}
+                  </Stretch>
+                  {/* 底部信息 */}
+                  <Fixed defaultSize={200} position={'top'}>
                     <LogsView store={globalLoggerStore} />
-                </Fixed>
-              </Stretch>
+                  </Fixed>
+                </Stretch>
 
-              <Fixed
-                defaultSize={200}
-                position={'left'}
-                style={{
-                  display: 'flex',
-                  flexDirection: 'column',
-                  justifyContent: 'stretch',
-                  alignItems: 'stretch',
-                }}
-              >
-                {/* 组件树 */}
-                <Stretch>
-                  <Panel>
-                    <WidgetTree
+                <Fixed
+                  defaultSize={200}
+                  position={'left'}
+                  style={{
+                    display: 'flex',
+                    flexDirection: 'column',
+                    justifyContent: 'stretch',
+                    alignItems: 'stretch',
+                  }}
+                >
+                  {/* 组件树 */}
+                  <Stretch>
+                    <Panel>
+                      {/* <WidgetTree
                       widgets={state.widgets}
                       childrenMap={state.childrenMap}
                       hoverId={state.hoverId}
                       rootId={state.rootId}
                       selectedIds={state.selectedIds}
-                    />
-                  </Panel>
-                </Stretch>
-                {/* 组件编辑 */}
-                <Fixed defaultSize={300} position={'top'}>
-                  <Panel style={{ padding: 8 }}>
-                    {state.selectedIds.length === 1 && (
+                    /> */}
+                    </Panel>
+                  </Stretch>
+                  {/* 组件编辑 */}
+                  <Fixed defaultSize={300} position={'top'}>
+                    <Panel style={{ padding: 8 }}>
+                      {/* {state.selectedIds.length === 1 && (
                       <WidgetEditor
                         widget={state.widgets[state.selectedIds[0]]}
                       />
-                    )}
-                  </Panel>
+                    )} */}
+                    </Panel>
+                  </Fixed>
                 </Fixed>
-              </Fixed>
-            </Stretch>
-          </div>
-          {/* 右键菜单 */}
-          <ContextMenu state={ctxMenuState} control={ctxMenuControl} />
-        </ContextMenuContext.Provider>
-      </DndProvider>
-    </VisualDispatcherContext.Provider>
+              </Stretch>
+            </div>
+            {/* 右键菜单 */}
+            <ContextMenu state={ctxMenuState} control={ctxMenuControl} />
+          </ContextMenuContext.Provider>
+        </DndProvider>
+      </PropertyEditorLibProvider>
+    </ComponentLibProvider>
+    // {/* </VisualDispatcherContext.Provider> */}
   );
 };
