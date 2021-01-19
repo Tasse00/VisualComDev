6b2687d9310b5b7f1e0a80222a1038dfca81a42b
Carl.Zhang <tasse_00@163.com>
2021-01-19 23:09:10 +0800

    feat: InstanceGuide and hover

--- src/components/Editor/InstanceGuide.tsx
+++ src/components/Editor/InstanceGuide.tsx
@@ -7,31 +7,33 @@ const HoverGuide: React.FC<{
     [id: string]: Element;
   };
   border: string;
-}> = props => {
+}> = (props) => {
   const { targetId, domMap, border } = props;
 
-  // const [style, setStyle] = useState({ left: 0, top: 0, width: 0, height: 0 });
   const { left, top } = useContext(EditorContext);
 
   const style: React.CSSProperties = {};
   if (targetId && domMap[targetId]) {
     const dom = domMap[targetId];
     const rect = dom.getBoundingClientRect();
-  
-    style.left= rect.left - left;
+
+    style.left = rect.left - left;
     style.top = rect.top - top;
     style.width = rect.width;
     style.height = rect.height;
   }
   return targetId ? (
-    <div style={{
-      position: 'absolute',
-      boxSizing: 'border-box',
-      border,
-      pointerEvents: 'none',
-      ...style
-    }}></div>
-  ) : null
-}
+    <div
+      style={{
+        position: 'absolute',
+        boxSizing: 'border-box',
+        transition: 'all 100ms',
+        border,
+        pointerEvents: 'none',
+        ...style,
+      }}
+    ></div>
+  ) : null;
+};
 
 export default HoverGuide;
--- src/components/Editor/InstanceHelper.tsx
+++ src/components/Editor/InstanceHelper.tsx
@@ -5,19 +5,21 @@ import ReactDOM from 'react-dom';
 import { ComponentLibContext } from '../ComponentLib/context';
 import { ComponentDragItem, DragItems, InstanceDragItem } from '../DragAndDrop';
 import { globalLoggerStore } from '../Globals';
-import { EditorContext, EditorDispatcherContext } from './context';
+import { EditorDispatcherContext } from './context';
 
 const logger = globalLoggerStore.createLogger('editor.helper');
 
 const InstanceHelper: React.FC<{
   node: VCD.ComponentInstanceTree;
-}> = props => {
+}> = (props) => {
   const { node } = props;
-  const { state: { componentsMap } } = useContext(ComponentLibContext);
+  const {
+    state: { componentsMap },
+  } = useContext(ComponentLibContext);
 
   const com = componentsMap[node.comId];
   if (!com) {
-    logger.error("invalid componentId:", node.comId);
+    logger.error('invalid componentId:', node.comId);
     return null;
   }
 
@@ -29,7 +31,7 @@ const InstanceHelper: React.FC<{
   useEffect(() => {
     const dom = ReactDOM.findDOMNode(ref.current);
     if (!dom) {
-      logger.warning("dom of instance:", node.guid, 'is not mounted!');
+      logger.warning('dom of instance:', node.guid, 'is not mounted!');
       return;
     }
     // 同步 dom 节点
@@ -38,12 +40,12 @@ const InstanceHelper: React.FC<{
       payload: {
         instanceId: node.guid,
         dom: dom as Element,
-      }
+      },
     });
 
     // 注册 over 回调： 悬浮状态
     const onMouseOver = (e: Event) => {
-      e.stopPropagation()
+      e.stopPropagation();
       dispatch({ type: 'hover-instance', payload: { instanceId: node.guid } });
     };
     dom.addEventListener('mouseover', onMouseOver);
@@ -51,17 +53,15 @@ const InstanceHelper: React.FC<{
     // 注册 click 回调： 选中状态
     const onClick = (e: Event) => {
       e.stopPropagation();
-      dispatch({ type: 'select-instance', payload: { instanceId: node.guid } })
-    }
+      dispatch({ type: 'select-instance', payload: { instanceId: node.guid } });
+    };
     dom.addEventListener('click', onClick);
 
     return () => {
       dom.removeEventListener('mouoseover', onMouseOver);
       dom.removeEventListener('click', onClick);
     };
-
   }, [node.guid]);
-
 
   // 拖拽
   const [{ isOverCurrent }, drop] = useDrop<
@@ -82,7 +82,8 @@ const InstanceHelper: React.FC<{
           payload: { parentId: node.guid, comId: item.data.guid },
         });
       } else if (
-        item.type === DragItems.Instance && item.data.guid !== node.guid
+        item.type === DragItems.Instance &&
+        item.data.guid !== node.guid
       ) {
         dispatch({
           type: 'move-instance',
@@ -91,7 +92,10 @@ const InstanceHelper: React.FC<{
       }
     },
     canDrop: (item, monitor) => {
-      return !!componentsMap[node.comId].isContainer && monitor.isOver({ shallow: true });
+      return (
+        !!componentsMap[node.comId].isContainer &&
+        monitor.isOver({ shallow: true })
+      );
     },
     collect: (monitor) => ({
       isOverCurrent: monitor.isOver({ shallow: true }),
@@ -100,41 +104,40 @@ const InstanceHelper: React.FC<{
 
   drop(ref);
 
-  const { left, top } = useContext(EditorContext);
-
   const style: React.CSSProperties = {};
+
   if (ref.current && isOverCurrent) {
     const rect = ref.current.getBoundingClientRect();
-    console.log(node.name, rect);
-
-    // style.left = rect.left - left;
-    // style.top = rect.top - top;
+    style.left = rect.left;
+    style.top = rect.top;
     style.width = rect.width;
     style.height = rect.height;
-    style.marginTop = - rect.height;
-
-    // ! 通过绝对定位会遇到相对定位父亲不可知的问题
-
-    // @TODO 通过js找父级!!!
   }
 
   return (
     <>
       <Com ref={ref}>
-        {(node.children || []).map(child => <InstanceHelper key={child.guid} node={child} />)}
+        {(node.children || []).map((child) => (
+          <InstanceHelper key={child.guid} node={child} />
+        ))}
       </Com>
-      {
-        <div style={{
-          pointerEvents: 'none',
-          position: 'absolute',
-          backgroundColor: 'rgba(0,0,255,0.2)',
-          ...style,
-        }}>
-          {style.left}x{style.top}
+      {isOverCurrent && (
+        <div
+          style={{
+            pointerEvents: 'none',
+            position: 'fixed',
+            backgroundColor: 'rgba(0,0,255,0.2)',
+            display: 'flex',
+            justifyContent: 'center',
+            alignItems: 'center',
+            ...style,
+          }}
+        >
+          {node.name}
         </div>
-      }
+      )}
     </>
   );
-}
+};
 
 export default InstanceHelper;
